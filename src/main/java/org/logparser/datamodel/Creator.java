89a144f605b75982960e2457302f923243249ea3
chinonye <chinonye@chinonysmacbook>
2018-03-04 20:47:05 -0600

    setting up project

--- src/main/java/org/logparser/datamodel/Creator.java
+++ src/main/java/org/logparser/datamodel/Creator.java
@@ -3,7 +3,27 @@ package org.logparser.datamodel;
 import java.util.Date;
 
 public class Creator {
+  String name;
+  Date date;
   
-  public Creator(final String name) {
+  public Creator(final String name, final Date date) {
+	  this.name = name;
+	  this.date = date;
+  }
+  
+  public void setName(final String name) {
+	  this.name = name;
+  }
+  
+  public void setDate(final Date date) {
+	  this.date = date;
+  }
+  
+  public String getName() {
+	  return name;
+  }
+  
+  public Date getDate() {
+	  return date;
   }
 }
